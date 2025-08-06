f453f8a7a481a9b76c902cd0d0e43254d2c74d61
pakar <pakar@192.168.1.13>
2025-08-07 03:00:09 +0900

    7.8	変数② 提出

--- kadai hensu1/src/curriculumA/Qes1.java
+++ kadai hensu1/src/curriculumA/Qes1.java
@@ -25,13 +25,41 @@ public class Qes1 {
 		isBoolean = true;
 		//ここまでQes2  7.7 変数①
 
-		System.out.println(byteNum + shortNum + intNum + longNum);
-		System.out.println(byteNum * 2);
-		System.out.println(letter + letters + isBoolean);
+		System.out.println(byteNum + shortNum + intNum + longNum);//11110を出力
+		System.out.println(byteNum * 2);//11110を出力
+		System.out.println(letter + letters + isBoolean);//a ハロー trueを出力
 		System.out.println(byteNum + shortNum + intNum + longNum + floatNum + doubleNum);//小数点表示
-		System.out.println(byteNum * shortNum * intNum * longNum);
-		System.out.println(doubleNum / shortNum);
-		System.out.println(byteNum - shortNum);
+		System.out.println(byteNum * shortNum * intNum * longNum);//数字を全て足して出力
+		System.out.println(doubleNum / shortNum);//10.5割る100をして出力
+		System.out.println(byteNum - shortNum);//10引く100をすして出力
+		
+		//ここまでQes3  7.7 変数②
+		
+		String name = null;//name という String型の変数 を宣言
+		name = "山田太郎";//name変数に"山田太郎"という値を代入
+		System.out.println("こんにちは"+name+"さん！");//name変数を使用してコンソールに こんにちは、山田太郎さん！ と表示してください。
+		//ここまでQes4  7.7 変数②
+		
+		int age = 0 ;//age という int型の変数 を宣言し
+		age = 25 ;//age変数に 25 を代入してください。
+		System.out.println("年齢:"+age+"歳");//age変数を使用してコンソールに  年齢: 25歳 という表示してください。
+		//ここまでQes5  7.7 変数②
+		
+		
+		int num1 = 10;//num1 という int型の変数 を宣言し、10 を代入
+		int num2 = 5;//num2 という int型の変数 を宣言し、5 を代入
+		int sum =num1+num2;//num1 と num2 を足した結果をsumという変数に代入
+		System.out.println(sum);//蒸気をコンソールに表示
+		//ここまでQes6  7.7 変数②
+		
+		int score = 80;//score という int型の変数 を宣言し、80 を代入
+		score = score+20;//score に 20 を加えて、更新する
+		System.out.println("最終スコア:"+score);//最終スコア: 100 　をscoreを使用してコンソールに表示してください。
+		
+		//ここまでQes7  7.7 変数②
+		
+		
+		
 	}
 
 }
