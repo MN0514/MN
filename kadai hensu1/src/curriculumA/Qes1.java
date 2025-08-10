55024b0cecb94ffecfc5ce8940381470cc3bff8f
pakar <pakar@192.168.1.12>
2025-08-10 11:00:55 +0900

    Qes10

--- kadai hensu1/src/curriculumA/Qes1.java
+++ kadai hensu1/src/curriculumA/Qes1.java
@@ -33,32 +33,47 @@ public class Qes1 {
 		System.out.println(doubleNum / shortNum);//10.5割る100をして出力
 		System.out.println(byteNum - shortNum);//10引く100をすして出力
 		
-		//ここまでQes3  7.7 変数②
+		//ここまでQes3  7.8 変数②
 		
 		String name = null;//name という String型の変数 を宣言
 		name = "山田太郎";//name変数に"山田太郎"という値を代入
 		System.out.println("こんにちは"+name+"さん！");//name変数を使用してコンソールに こんにちは、山田太郎さん！ と表示してください。
-		//ここまでQes4  7.7 変数②
+		//ここまでQes4  7.8 変数②
 		
 		int age = 0 ;//age という int型の変数 を宣言し
 		age = 25 ;//age変数に 25 を代入してください。
 		System.out.println("年齢:"+age+"歳");//age変数を使用してコンソールに  年齢: 25歳 という表示してください。
-		//ここまでQes5  7.7 変数②
+		//ここまでQes5  7.8 変数②
 		
 		
 		int num1 = 10;//num1 という int型の変数 を宣言し、10 を代入
 		int num2 = 5;//num2 という int型の変数 を宣言し、5 を代入
 		int sum =num1+num2;//num1 と num2 を足した結果をsumという変数に代入
 		System.out.println(sum);//上記をコンソールに表示
-		//ここまでQes6  7.7 変数②
+		//ここまでQes6  7.8 変数②
 		
 		int score = 80;//score という int型の変数 を宣言し、80 を代入
 		score = score+20;//score に 20 を加えて、更新する
 		System.out.println("最終スコア:"+score);//最終スコア: 100 をscoreを使用してコンソールに表示してください。
+		//ここまでQes7  7.8 変数②
 		
-		//ここまでQes7  7.7 変数②
 		
+		double price = 99.99 ;//price という double型の変数 を宣言し、99.99 を代入
+		int P = (int)price ;//price を int型 に変換
+		System.out.println("整数価格:" + P );//整数価格: 99 とコンソールに表示
+		//ここまでQes8  7.9 型変換
 		
+		String numStr = null;//String 型の変数 numStr を宣言
+		numStr = "123" ;//変数 numStr に "123" を代入
+		int str = Integer.parseInt(numStr); // 文字列を int に変換
+		System.out.println(str + 10 );//変換後の値: の後ろにnumStr + 10 した結果をコンソールに表示してください。
+		//ここまでQes9  7.9 型変換
+		
+		int num = 50 ;//int 型の変数 num に 50 を代入
+		String stri = String.valueOf(num);//num を String 型に変換
+		System.out.println("得点:" + stri + "点" );//"得点: 50点" の形で表示
+		
+		//ここまでQes10  7.9 型変換
 		
 	}
 
