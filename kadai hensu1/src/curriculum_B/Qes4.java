70a0b80a81fb88c5a6aa54d4892decb3d64e6ce1
pakar <pakar@192.168.1.10>
2025-09-06 00:00:28 +0900

    teisyutu

--- kadai hensu1/src/curriculum_B/Qes4.java
+++ kadai hensu1/src/curriculum_B/Qes4.java
@@ -12,9 +12,9 @@ public class Qes4 {
 		//Qe4 1
 		
 		int[] scores2 = {10,20,30,40,50};
-		for(int b = 4; b < scores2.length; b--){ //.lengthで配列の長さ分だけ繰り返す
+		for(int b = scores2.length -1 ; b >= 0 ; b--){ //.lengthで配列の長さ分だけ繰り返す
 			System.out.println("scores2["+ b +"]の値：" + scores2[b]);
-			sum -= scores1[b];
+			sum -= scores2[b];
 			}
 		//Qe4 2
 		
